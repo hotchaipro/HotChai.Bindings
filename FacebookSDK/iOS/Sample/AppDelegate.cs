df2158907538015c3e6ed475655cc9508c77e0dc
dtaylorus <dtaylorus@hotmail.com>
2022-11-19 06:00:54 -0500

    FacebookSDK bindings for iOS and sample project

---
+++ FacebookSDK/iOS/Sample/AppDelegate.cs
@@ -0,0 +1,39 @@
+namespace iOS;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate
+{
+	public override UIWindow? Window
+	{
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching(
+		UIApplication application,
+		NSDictionary launchOptions)
+	{
+        Facebook.CoreKit.ApplicationDelegate.InitializeSdk(launchOptions);
+
+        Facebook.CoreKit.ApplicationDelegate.SharedInstance.FinishedLaunching(application, launchOptions);
+
+		// create a new window instance based on the screen size
+        Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController();
+		vc.View!.AddSubview(new UILabel (Window!.Frame)
+		{
+			BackgroundColor = UIColor.SystemBackground,
+			TextAlignment = UITextAlignment.Center,
+			Text = "Hello, iOS!",
+			AutoresizingMask = UIViewAutoresizing.All,
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible();
+
+        return true;
+	}
+}
