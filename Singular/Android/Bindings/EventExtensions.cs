aaa91bc95d37f66819a91c6dcb785fd050806033
David Taylor <dtaylorus@hotmail.com>
2025-03-02 19:18:27 -0500

    [Singular] Updated Android bindings

---
+++ Singular/Android/Bindings/EventExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Com.Singular.Sdk
+{
+    public partial class Singular
+    {
+        public static void Event(
+            Events name)
+        {
+            Singular.Event(name.Name());
+        }
+
+        public static void Event(
+            Events name,
+            string extra)
+        {
+            Singular.Event(name.Name(), extra);
+        }
+
+        public static void EventJSON(
+            Events name,
+            global::Org.Json.JSONObject json)
+        {
+            Singular.EventJSON(name.Name(), json);
+        }
+    }
+}
