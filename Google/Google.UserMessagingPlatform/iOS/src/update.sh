c3e452a24a1b00c1619b16263a5ae1843c01066e
David Taylor <dtaylorus@hotmail.com>
2025-03-15 13:45:52 -0400

    [UserMessagingPlatform] Move iOS bindings to a separate project

--- Google/Google.UserMessagingPlatform/iOS/src/update.sh
+++
@@ -1,5 +0,0 @@
-﻿# CocoaPods: https://cocoapods.org
-pod update
-
-# Sharpie: http://xmn.io/sharpie-docs
-sharpie bind -sdk iphoneos17.2 -f ./Pods/GoogleUserMessagingPlatform/Frameworks/Release/UserMessagingPlatform.xcframework/ios-arm64/UserMessagingPlatform.framework -namespace Google.UserMessagingPlatform
