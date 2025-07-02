db231042569e379fa70f1cf0a8897fcb66a05aca
David Taylor <dtaylorus@hotmail.com>
2025-07-02 11:20:48 -0400

    (Android) Separate project for Facebook Core bindings

---
+++ Facebook/Core/Android/Additions/GraphRequestAsyncTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Android.Runtime;
+
+namespace Com.Facebook
+{
+    public partial class GraphRequestAsyncTask
+    {
+        protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
